5de99f5773c2ff267be2a24c1e0edfd9e8e22952
STURMKAROTTE-coder <145476866+STURMKAROTTE-coder@users.noreply.github.com>
2025-05-02 23:49:30 +0200

    git

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -23,7 +23,11 @@ public class Player_Movement : MonoBehaviour
     void Movement()
     {
         float t = ProjectileSimulation.s;
-        float totalPoints = lr.positionCount - 1;
+        t = t * 5;
+        float totalPoints = 0;
+
+        totalPoints = lr.positionCount - 1;
+
 
         if (lr == null || lr.positionCount < 2) return;
 
@@ -41,11 +45,15 @@ public class Player_Movement : MonoBehaviour
         int index = Mathf.FloorToInt(punkt);
         float lerpT = punkt - index;
 
-        if (index < totalPoints)
+        Vector3 p1 = lr.GetPosition(index);
+        Vector3 p2 = lr.GetPosition(index + 1);
+        transform.position = Vector3.Lerp(p1, p2, lerpT);
+        Vector3 direction = (p2 - p1).normalized;
+        if (direction != Vector3.zero)
         {
-            Vector3 p1 = lr.GetPosition(index);
-            Vector3 p2 = lr.GetPosition(index + 1);
-            transform.position = Vector3.Lerp(p1, p2, lerpT);
+            transform.rotation = Quaternion.LookRotation(direction);
         }
+
+
     }
 }
