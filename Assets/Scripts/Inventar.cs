e4c1277c9b42cf30f372e26d8c521591c4a89adc
STURMKAROTTE-coder <145476866+STURMKAROTTE-coder@users.noreply.github.com>
2025-05-04 14:19:13 +0200

    zt

---
+++ Assets/Scripts/Inventar.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class Inventar : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        List inventar = new List();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void InventarSpieler()
+    {
+
+    }
+}
